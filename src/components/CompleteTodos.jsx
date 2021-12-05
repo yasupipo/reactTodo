45be84905322b16559be0f5f92e4587110242b0b
yasupipo <53911164+yasupipo@users.noreply.github.com>
2021-12-05 17:20:22 +0900

    Initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { completeTodos, onClickRev } = props;
+  return (
+    <>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo, index) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button onClick={() => onClickRev(index)}>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
